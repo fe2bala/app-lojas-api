b40a520cc07e8c3e69df0fb0b67ed7e6ce13cbe2
Felipe de Almeida Balascio <felipe.balascio@ituran.com.br>
2021-10-30 19:32:42 -0300

    ajuste

--- src/Services/auth.service.ts
+++ src/Services/auth.service.ts
@@ -1,17 +1,26 @@
 import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import api from 'src/Services/api';
-
+import { JSEncrypt } from 'nodejs-jsencrypt';
 
 @Injectable()
 export class AuthService {
     
+    private _jsEncrypt: JSEncrypt;
 
+    get JSEncrypt(): JSEncrypt {
+        if (this._jsEncrypt) {
+          return this._jsEncrypt;
+        }
+        this._jsEncrypt = new JSEncrypt();
+        this._jsEncrypt.setPublicKey('MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDVTUS+4BhoQ/s4ukE/8Hu1n4UK9UbRNnrsSxfkiagb5oSzVnt3igDJSz8JH18pYrkL40pEaGqqxIaDozJke8APiHJRzn8FS9psH2PfdTnsmXGGuQfnrlSjYHBIsmghX2uLSoSy4NgPOgitUsjLPrSsCPsyT14LA0vc+UbVG7J8zQIDAQAB');
+        return this._jsEncrypt;
+    }
 
     async token(username: string, password: string) {
         return api.post(
         `autenticacao/token`, {
-        username: username,
-        password: password
+            username: this.JSEncrypt.encrypt(username),
+            password: this.JSEncrypt.encrypt(password)
         }).then(response=> {
             return response.data;
         }).catch(error=> {
